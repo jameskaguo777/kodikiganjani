9e1aaa25c1e341384da75e95ebcebbd3e2c8ddec
jameskaguo777 <jameskaguo@live.com>
2020-08-01 03:05:03 +0300

    update14

--- app/Http/Controllers/NotificationCenterController.php
+++ app/Http/Controllers/NotificationCenterController.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Storage;
 class NotificationCenterController extends Controller
 {
     //
+    
 
     public function index(){
         $notifications = NotificationCenter::orderByDesc('id')->get();
@@ -24,6 +25,43 @@ class NotificationCenterController extends Controller
             'featured_image_url' => 'required|image',
         ]);
 
+        
+
+        $url = 'https://fcm.googleapis.com/fcm/send';
+
+        $fields = array (
+                'priority' => 'high',
+                'to' => '/topics/all',
+                'title' => $request->title,
+                'body' => $request->summary,
+                'time_to_live' => 600,
+                // 'click_action'=> 'FLUTTER_NOTIFICATION_CLICK',
+                'data' => array (
+                    'click_action'=> 'FLUTTER_NOTIFICATION_CLICK'
+                )
+        );
+
+
+        $fields = json_encode ( $fields );
+
+        $headers = array (
+                'Authorization: key=' . "AAAAdPJ82Z8:APA91bGbgcM0aXOw8GDuJWEEccsvhp9ZiDu1wcDELgREn-tt1z0DzlgD622nFgJ6zdTmp9TgTMhUNZdjR4P1MzeTce6h_ERFB3Eub7-aTlb7A1ig_2ECmnc5O5RRfBly04HQLvjUf71i",
+                'Content-Type: application/json'
+        );
+
+        $ch = curl_init ();
+        curl_setopt ( $ch, CURLOPT_URL, $url );
+        curl_setopt ( $ch, CURLOPT_POST, true );
+        curl_setopt ( $ch, CURLOPT_HTTPHEADER, $headers );
+        curl_setopt ( $ch, CURLOPT_RETURNTRANSFER, true );
+        curl_setopt ( $ch, CURLOPT_POSTFIELDS, $fields );
+
+        $result = curl_exec ( $ch );
+        // echo $result;
+        curl_close ( $ch );
+
+
+
         $path = $request->file('featured_image_url')->store('notifications');
 
         NotificationCenter::create([
@@ -35,7 +73,7 @@ class NotificationCenterController extends Controller
         ]);
         $messages = array(
             'status' => 'success',
-            'message' => 'Notification Posted Successful',
+            'message' => 'Notification Posted Successful '. $result ,
         );
         return redirect(route('noti-index'))->with('status', $messages);
     }
